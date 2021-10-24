87770284c123dc3887873536b18476606ded7551
Stefan Helmer <sh@validio.de>
2021-10-24 23:31:13 +0200

    Added google command via wsl

--- PSProfile.ps1
+++ PSProfile.ps1
@@ -1,15 +1,21 @@
-Function WhatIsMyIPCommand {Invoke-RestMethod ipinfo.io/ip}
-Function WttrCommand {Invoke-RestMethod http://wttr.in}
-Function SpeedtestCommand {wsl speedtest}
+Function WhatIsMyIPCommand { Invoke-RestMethod ipinfo.io/ip }
+Function WttrCommand { Invoke-RestMethod http://wttr.in }
+Function SpeedtestCommand { wsl speedtest }
+Function Googler() {
+    $search = $args -join " "
+    wsl googler $search
+}
 
 Set-Alias -Name whatismyip -Value WhatIsMyIPCommand
 Set-Alias -Name wttr -Value WttrCommand
 Set-Alias -Name speedtest -Value SpeedtestCommand
+Set-Alias -Name google -Value Googler
+
 Import-Module PowerColorLS
 
 Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+        $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell) -join "`n"
-})
+    })
 
 oh-my-posh --init --shell pwsh --config ~\prompt\rockschtar.hunk.omp.json | Invoke-Expression
